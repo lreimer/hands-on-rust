14f1f043ac25951eae14a9a4ce5403a6669b5811
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-01-27 22:25:45 +0100

    Query PodList for namespace via K8s API.

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Hands-on Rust
 
-Demo repository for my Rust language experiments.
+Demo repository for my Rust language experiments. Basically, this is a simple REST client
+to call the Kubernetes API and retrieve the list of pods for a namespace.
+
+## Building and Running
+
+```bash
+$ kubectl proxy &
+$ cargo run -- kube-system
+```
 
 ## Maintainer
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate log;
 use env_logger::Env;
 use structopt::StructOpt;
 
+use serde::{Deserialize};
+
 /// Display list of pods in Kubernetes namespace
 #[derive(StructOpt)]
 struct Cli {
@@ -11,9 +13,39 @@ struct Cli {
     namespace: String,
 }
 
-fn main() {
+/// JSON definition of a PodList
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PodList {
+    kind: String,
+    api_version: String,
+    items: Vec<Item>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Item {
+    metadata: Metadata,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Metadata {
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     
     let args = Cli::from_args();
     info!("Getting pods for namespace {}", &args.namespace);
+
+    let request_url = format!("http://localhost:8001/api/v1/namespaces/{namespace}/pods",
+                              namespace = &args.namespace);
+    let response = reqwest::get(&request_url).await?;
+
+    let pods: PodList = response.json().await?;
+    println!("{:?}", pods);
+    Ok(())
 }
