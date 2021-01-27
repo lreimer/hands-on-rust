9a5f727fe04d1e57cc96548ba47344eef8df243e
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-01-27 21:32:44 +0100

    Initial working app.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate log;
+
+use env_logger::Env;
+use structopt::StructOpt;
+
+/// Display list of pods in Kubernetes namespace
+#[derive(StructOpt)]
+struct Cli {
+    /// The Kubernetes namespace
+    namespace: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    
+    let args = Cli::from_args();
+    info!("Getting pods for namespace {}", &args.namespace);
 }
